0e1fce7cbe47873d7112d96770c53bcde7c5d956
giorgi-bezhaishvili <giorgibezhiashvili5@gmail.com>
2026-04-29 14:00:15 +0400

    Now you can Log in too

--- index.js
+++ index.js
@@ -47,14 +47,33 @@ app.post("/register",[body('mail').isEmail().withMessage(`Please enter valid ema
         email:mail
     }
     users.push(newUser);
+    res.send(`User Created succesfully`)
     saveFileData(users)
 }catch(err){
     console.log(err);
-    res.status(404).send(`Server Error`)
-    return []
+    return res.status(404).send(`Server Error`)
 }
 })
-
+app.post(`/login` , async (req,res)=>{
+  try{
+    const {userName , email , password} =  req.body
+    const users = getFileData()
+    const user = users.find((u) => 
+    userName ? u.userName === userName : u.mail === mail
+  );
+    if(user === undefined){
+      return res.send(`Username or password isnt correct`)
+    }
+    const isMatch = await bcrypt.compare(password,user.password)
+    if(!isMatch){
+      return res.send(`Invalid username or password`)
+    }
+    res.send(`Is match`)
+  }catch(err){
+    console.log(err);
+    res.status(404).send(`server error`)
+  }
+})
 const server = https.createServer((httpsNeccecities), app)
 server.listen(8080, (req,res)=>{
     console.log(`Server is spinning on port 3000`)
