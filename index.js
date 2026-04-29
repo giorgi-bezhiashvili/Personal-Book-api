abe474dd6614931e678c575cb71baf87cab50ee0
giorgi-bezhaishvili <giorgibezhiashvili5@gmail.com>
2026-04-29 14:40:26 +0400

    Added Helmet and Joi fixed bugs also.

--- index.js
+++ index.js
@@ -1,85 +1,114 @@
-const express = require(`express`)
-const app = express()
-const fs = require(`fs`)
-const path = require(`path`)
-const bcrypt = require(`bcrypt`)
-const key = fs.readFileSync(path.join(__dirname , `localhost.key`))
-const cert = fs.readFileSync(path.join(__dirname , `localhost.crt`))
-const https = require(`https`)
-const httpsNeccecities = {key:key , cert:cert}
-const { query, validationResult, body } = require('express-validator');
-const jwt = require('jsonwebtoken');
-const dotenv = require('dotenv');
+const express = require(`express`);
+const app = express();
+const fs = require(`fs`);
+const path = require(`path`);
+const bcrypt = require(`bcrypt`);
+const key = fs.readFileSync(path.join(__dirname, `localhost.key`));
+const cert = fs.readFileSync(path.join(__dirname, `localhost.crt`));
+const https = require(`https`);
+const httpsNeccecities = { key: key, cert: cert };
+const { query, validationResult, body } = require("express-validator");
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+const Joi = require(`joi`);
+const helmet = require(`helmet`);
 dotenv.config();
+app.disable("x-powered-by");
+
+app.use(
+  helmet({
+    xPoweredBy: false,
+  }),
+);
+
+const loginSceme = Joi.object({
+  email: Joi.string().email().required(),
+  password: Joi.string().min(8).max(20).required(),
+  userN: Joi.string().alphanum(),
+}).xor(`userN`, "email");
 
 app.use(express.json());
 const getFileData = () => {
   try {
-    return JSON.parse(fs.readFileSync(path.join(__dirname,`data.json`), "utf8"));
+    return JSON.parse(
+      fs.readFileSync(path.join(__dirname, `data.json`), "utf8"),
+    );
   } catch {
     return [];
   }
 };
 const saveFileData = (data) => {
-  fs.writeFileSync(path.join(__dirname,`data.json`), JSON.stringify(data, null, 2));
+  fs.writeFileSync(
+    path.join(__dirname, `data.json`),
+    JSON.stringify(data, null, 2),
+  );
 };
 
+app.post(
+  "/register",
+  [
+    body("mail").isEmail().withMessage(`Please enter valid email`),
+    body(`userName`).notEmpty().withMessage(`Username is required`),
+    body(`password`).isLength({ min: 8 }).withMessage(`Password too short`),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { userName, mail, password } = req.body;
 
-app.post("/register",[body('mail').isEmail().withMessage(`Please enter valid email`),
-  body(`userName`).notEmpty().withMessage(`Username is required`),
-  body(`password`).isLength({min:8}).withMessage(`Password too short`)], async(req,res)=>{
-  
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
+    const users = getFileData();
+    try {
+      if (users.find((u) => u.userName === userName)) {
+        return res.status(404).send(`User already exists`);
+      }
+      if (users.find((u) => u.mail === mail)) {
+        return res.status(404).send(`User already exists`);
+      }
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const newUser = {
+        userName: userName,
+        password: hashedPassword,
+        email: mail,
+      };
+      users.push(newUser);
+      res.send(`User Created succesfully`);
+      saveFileData(users);
+    } catch (err) {
+      console.log(err);
+      return res.status(404).send(`Server Error`);
+    }
+  },
+);
+app.post(`/login`, async (req, res) => {
+  const { error, value } = loginSceme.validate(req.body);
+  if (error) {
+    console.log(error);
+    return res.send(`Invalid request`);
   }
-  const {userName,mail,password} = req.body
-    
-  const users = getFileData()
-   try{
-    if(users.find((u)=>u.userName===userName)){
-        return res.status(404).send(`User already exists`)
+  try {
+    const { userName, email, password } = req.body;
+    const users = getFileData();
+    const user = users.find((u) =>
+      userName ? u.userName === userName : u.email === email,
+    );
+    if (user === undefined) {
+      return res.send(`Username or password isnt correct`);
     }
-    if(users.find((u)=>u.mail===mail)){
-        return res.status(404).send(`User already exists`)
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.send(`Invalid username or password`);
     }
-    const hashedPassword = await bcrypt.hash(password,10)
-    const newUser = {
-        userName:userName,
-        password:hashedPassword,
-        email:mail
-    }
-    users.push(newUser);
-    res.send(`User Created succesfully`)
-    saveFileData(users)
-}catch(err){
+    const accesToken = await jwt.sign(user, process.env.ACCES_TOKEN_SECRET);
+    res.json({ isMatch, accesToken });
+  } catch (err) {
     console.log(err);
-    return res.status(404).send(`Server Error`)
-}
-})
-app.post(`/login` , async (req,res)=>{
-  try{
-    const {userName , email , password} =  req.body
-    const users = getFileData()
-    const user = users.find((u) => 
-    userName ? u.userName === userName : u.mail === mail
-  );
-    if(user === undefined){
-      return res.send(`Username or password isnt correct`)
-    }
-    const isMatch = await bcrypt.compare(password,user.password)
-    if(!isMatch){
-      return res.send(`Invalid username or password`)
-    }
-    const accesToken = await jwt.sign(user,process.env.ACCES_TOKEN_SECRET)
-    res.json({isMatch , accesToken})
-  }catch(err){
-    console.log(err);
-    res.status(404).send(`server error`)
+    res.status(404).send(`server error`);
   }
-})
+});
 
-const server = https.createServer((httpsNeccecities), app)
-server.listen(8080, (req,res)=>{
-    console.log(`Server is spinning on port 3000`)
-})
+const server = https.createServer(httpsNeccecities, app);
+server.listen(8080, (req, res) => {
+  console.log(`Server is spinning on port 3000`);
+});
